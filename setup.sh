82a2a72bcb907fd78f99af2b4ec1396d9240a758
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-05 13:11:28 +0200

    Added setup.sh to easily install it

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Making init.sh executable...
+chmod +x init.sh
+
+echo Copying reverse shells into /opt/reverse_shells/
+cp -r reverse_shells/ /opt/reverse_shells/
+
+echo Creating symlink for init.sh
+ln -s init.sh /sbin/initWorkspace
+
+echo Everything\'s setup! Simply run initWorkspace to get started!
