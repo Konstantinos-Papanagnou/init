693085b57bfb6e0c093b02c30866752508e9adbc
konstantinos Pap <konstantinos@localhost.localdomain>
2021-08-07 08:34:47 -0500

    More Features

--- init.sh
+++ init.sh
@@ -1,64 +1,72 @@
 #!/bin/bash
 
-#Check for 2 arguments and require them
+#Check for 3 arguments and require them
 
+export machine_name=$1
+export ip=$2
+export ext=$3
 if [ -z $1 ]
   then
-    echo "Syntax: ./init {machine_name} {ip}"
+    echo "Syntax: ./init {machine_name} {ip} {extension (thm, htb, etc) Default: thm}"
     exit
 fi
 if [ -z $2 ] 
   then
-    echo "Syntax: ./init {machine_name} {ip}"
+    echo "Syntax: ./init {machine_name} {ip} {extension (thm, htb, etc) Default: thm}"
     exit
+fi
+if [ -z $3 ] 
+  then
+    ext="thm"
 fi
 
 echo "Adding /etc/hosts entry"
-echo $ip $machine_name.htb | sudo tee -a /etc/hosts
-
-#cd ~/Documents
+echo $ip $machine_name.$ext | sudo tee -a /etc/hosts
 
 echo 'Creating folders...'
-mkdir -p $1/nmap
+mkdir -p $machine_name/nmap
 
-cd $1
+cd $machine_name
 
 echo 'Setting up basic files...'
 touch creds
 touch README.md
 
-echo "# $1" > README.md
-echo "## export IP=$2" >> README.md
+echo "# $machine_name" > README.md
+echo "## export IP=$ip" >> README.md
 echo " " >> README.md
 now=$(date)
 echo " > Konstantinos Pap - $now" >> README.md
 cp /opt/reverse_shells/misc.txt .
 cp /opt/reverse_shells/reverse.php .
-ip=$(ip addr show | grep tun0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-echo "Your vpn ip is: $ip"
+myip=$(ip addr show | grep tun0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+echo "Your vpn ip is: $myip"
 
-sed -i "s/{ip}/$ip/" reverse.php
-sed -i "s/{ip}/$ip/" misc.txt
+sed -i "s/{ip}/$myip/" reverse.php
+sed -i "s/{ip}/$myip/" misc.txt
 
 echo 'Everything is set up'
 
-echo "Your gobuster syntax is: gobuster dir -x php,html,sh,bin,txt -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -u http://$2 | tee gobuster.log"
+echo "Your gobuster syntax is: gobuster dir -x php,html,sh,bin,txt -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -u http://$ip | tee gobuster.log"
 echo "Executing gobuster... Results saved at gobuster.log"
-xterm -e bash -c "gobuster dir -x php,html,sh,bin,txt -w/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -u http://$2 --wildcard | tee gobuster.log" &
+xterm -e bash -c "gobuster dir -x php,html,sh,bin,txt -w/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -u http://$ip --wildcard | tee gobuster.log" &
 
-echo "Nikto syntax: nikto --url http://$2 |tee nikto.log"
+echo "Nikto syntax: nikto --url http://$ip |tee nikto.log"
 echo "Executing nikto... Results saved at nikto.log"
-xterm -e bash -c "nikto --url http://$2 | tee nikto.log" &
+xterm -e bash -c "nikto --url http://$ip | tee nikto.log" &
 echo;echo;echo
 echo "Initializing nmap scan"
 
-sudo nmap -sS -sV -sC -vvv -oN nmap/initial $2
+echo "Firing up firefox on the website."
+firefox http://$machine_name.$ext &
+
+sudo nmap -sS -sV -sC -vvv -oN nmap/initial $ip
 subl README.md creds nmap/initial &
 
 echo;echo;echo;echo
 echo "Initializing all port scan"
 
-sudo nmap -sS -sV -sC -p- -oN nmap/allports $2
+sudo nmap -sS -sV -sC -p- -oN nmap/allports $ip
 
 echo "You can view the complete results of all port scan in nmap/allports"
 subl nmap/allports &
