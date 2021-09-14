dbb805c4fb8784f5c356174274dfb0494476dfb6
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-09-14 12:10:28 +0300

    Modify setup and resources location

--- setup.sh
+++ setup.sh
@@ -3,10 +3,20 @@
 echo Making init.sh executable...
 chmod +x init.sh
 
-echo Copying reverse shells into /opt/reverse_shells/
-cp -r reverse_shells/ /opt/reverse_shells/
+echo Creating Directories
+mkdir /opt/init
+mkdir /opt/init/reverse_shells
+
+echo Copying reverse shells into /opt/init/reverse_shells/
+cp -r reverse_shells/ /opt/init/
+
+echo Copying anonymouschecker.sh to /opt/init
+cp anonymouschecker.sh /opt/init/
 
 echo Creating symlink for init.sh
 ln -s $(pwd)/init.sh /bin/initWorkspace
+
+echo Creating symlink for manual_init.sh
+ln -s $(pwd)/manual_init.sh /bin/initmanualWorkspace
 
 echo Everything\'s setup! Simply run initWorkspace to get started!
